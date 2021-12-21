952cb85725871b184017369bd960ee25d241a090
Murat Arslan <mrterkmglsv@gmail.com>
2021-12-21 15:25:36 +0100

    OOP

---
+++ src/OOP/Constructor.java
@@ -0,0 +1,22 @@
+package OOP;
+
+public class Constructor {
+
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		Metotlar obj = new Metotlar();
+		
+		System.out.println(obj.Fen);
+		
+		
+		
+		
+		Metotlar obj2 = new Metotlar (75,80,"Ali");
+		System.out.println(obj2.Mat);
+	}
+
+}
---
+++ src/OOP/Metotlar.java
@@ -0,0 +1,36 @@
+package OOP;
+
+public class Metotlar {
+
+	int Mat=50;
+	int Fen=60;
+	String isim;
+	
+	static String OkulAdi="Java";
+
+	public Metotlar() {
+		System.out.println("Parametresiz");
+	}
+	
+	
+	public Metotlar(int Mat, int Fen, String isim) {
+		this();
+		this.Mat=Mat;
+		this.Fen=Fen;
+		this.isim=isim;
+		
+	}
+
+
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+	
+	
+	
+	
+}
